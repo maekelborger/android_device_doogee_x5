9a01a81e136415367943f85939425469433285be
Brint E. Kriebel <github@bekit.net>
2018-07-13 12:42:38 -0700

    patches: Switch patches to using git-am
    Also start a branch for the patched repos
    
    Change-Id: Ieefe1b3fd1663447b909246c31e5b6c6d86b174a

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 cd ../../../..
 cd system/core
-patch -p1 < ../../device/doogee/x5/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-patch -p1 < ../../device/doogee/x5/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
-patch -p1 < ../../device/doogee/x5/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
+repo start doogee-x5-patches .
+git am < ../../device/doogee/x5/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+git am < ../../device/doogee/x5/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
+git am < ../../device/doogee/x5/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
 cd ../..
 cd bionic
-patch -p1 < ../device/doogee/x5/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
+repo start doogee-x5-patches .
+git am < ../device/doogee/x5/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
 cd ..
 cd system/sepolicy
-patch -p1 < ../../device/doogee/x5/patches/0001-system_sepolicy.patch
+repo start doogee-x5-patches .
+git am < ../../device/doogee/x5/patches/0001-system_sepolicy.patch
 cd ../..
 cd frameworks/av
-patch -p1 < ../../device/doogee/x5/patches/0001-frameworks_av.patch
+repo start doogee-x5-patches .
+git am < ../../device/doogee/x5/patches/0001-frameworks_av.patch
 cd ../..
 cd frameworks/native
-patch -p1 < ../../device/doogee/x5/patches/frameworks_native.patch
+repo start doogee-x5-patches .
+git am < ../../device/doogee/x5/patches/frameworks_native.patch
 cd ../..
 cd frameworks/base
-patch -p1 < ../../device/doogee/x5/patches/frameworks_base.patch
-patch -p1 < ../../device/doogee/x5/patches/0007-fix-out-of-memory-gl-crashes.patch
+repo start doogee-x5-patches .
+git am < ../../device/doogee/x5/patches/frameworks_base.patch
+git am < ../../device/doogee/x5/patches/0007-fix-out-of-memory-gl-crashes.patch
 cd ../..
 cd system/netd
-patch -p1 < ../../device/doogee/x5/patches/0010-wifi-tethering-fix.patch
+repo start doogee-x5-patches .
+git am < ../../device/doogee/x5/patches/0010-wifi-tethering-fix.patch
 cd ../..
 cd external/wpa_supplicant_8
-patch -p1 < ../../device/doogee/x5/patches/0001-Ignore-fake-nvram-wifi.patch
-patch -p1 < ../../device/doogee/x5/patches/0005-fix-wlan-hidden-wifi-scan.patch
+repo start doogee-x5-patches .
+git am < ../../device/doogee/x5/patches/0001-Ignore-fake-nvram-wifi.patch
+git am < ../../device/doogee/x5/patches/0005-fix-wlan-hidden-wifi-scan.patch
 cd ../..
 cd packages/apps/Settings
-patch -p1 < ../../../device/doogee/x5/patches/0001-add-MiraVision-in-Settings.patch
+repo start doogee-x5-patches .
+git am < ../../../device/doogee/x5/patches/0001-add-MiraVision-in-Settings.patch
 cd ../../..
 
